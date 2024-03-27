49614ac8f8504f9c3c5d2382ad7211473b58a867
Sayed Abdul <sayed_abdul_karim@optum.com>
2024-03-27 12:54:11 +0530

    updated

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import {
   Dimensions,
   SafeAreaView,
@@ -6,10 +6,10 @@ import {
   Text,
   View,
   ScaledSize,
-} from 'react-native';
-import {getFontFamily} from '../assets/fonts/helper';
-import {horizontalScale, scaleFontSize} from '../utils/scaling';
-import {NavigationProp} from '@react-navigation/native';
+} from "react-native";
+import { getFontFamily } from "../assets/fonts/helper";
+import { horizontalScale, scaleFontSize } from "../utils/scaling";
+import { NavigationProp } from "@react-navigation/native";
 
 interface ScreenData {
   width: number;
@@ -22,25 +22,30 @@ type RootStackParamList = {
 };
 
 // Define the type for the navigation prop specific to this screen
-type HomeScreenNavigationProp = NavigationProp<RootStackParamList, 'Home'>;
+type HomeScreenNavigationProp = NavigationProp<RootStackParamList, "Home">;
 
 interface HomeProps {
   navigation: HomeScreenNavigationProp;
 }
 
-const Home: React.FC<HomeProps> = ({navigation}) => {
+const Home: React.FC<HomeProps> = ({ navigation }) => {
   const [screenData, setScreenData] = useState<ScreenData>(
-    Dimensions.get('screen'),
+    Dimensions.get("screen")
   );
 
   useEffect(() => {
     // let subscription: EmitterSubscription;
 
-    const onChange = ({screen}: {window: ScaledSize; screen: ScaledSize}) => {
+    const onChange = ({
+      screen,
+    }: {
+      window: ScaledSize;
+      screen: ScaledSize;
+    }) => {
       setScreenData(screen);
     };
 
-    const subscription = Dimensions.addEventListener('change', onChange);
+    const subscription = Dimensions.addEventListener("change", onChange);
 
     return () => subscription.remove();
   }, []);
@@ -51,20 +56,22 @@ const Home: React.FC<HomeProps> = ({navigation}) => {
         <View
           style={[
             styles.test,
-            {height: screenData.height / 2, width: screenData.width / 2},
-          ]}>
+            { height: screenData.height / 2, width: screenData.width / 2 },
+          ]}
+        >
           {/* <Text style={styles.text}>Hello worlds</Text> */}
           <Text style={styles.text}>111111</Text>
           <Text style={styles.text2}> 11</Text>
         </View>
 
-        <Text style={styles.text}>Hello worldxxx</Text>
+        <Text style={styles.text}>Hello worlddddxxx</Text>
         <Text style={styles.text2}>Hello world 22</Text>
         {/* <Text onPress={() => navigation.navigate(Routes.About)}>About</Text> */}
         <Text
           onPress={() => {
-            navigation.navigate('About');
-          }}>
+            navigation.navigate("About");
+          }}
+        >
           Abouttt
         </Text>
       </View>
@@ -77,23 +84,23 @@ const styles = StyleSheet.create({
   container: {
     // flex: 1,
     borderWidth: 1,
-    borderColor: 'red',
+    borderColor: "red",
   },
   test: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'blue',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "blue",
   },
   text: {
     fontSize: scaleFontSize(20),
-    fontFamily: getFontFamily('Inter', '800'), // Assuming this function returns a string
-    fontWeight: 'bold',
+    fontFamily: getFontFamily("Inter", "800"), // Assuming this function returns a string
+    fontWeight: "bold",
     marginLeft: horizontalScale(20),
   },
   text2: {
     fontSize: 20,
-    fontFamily: getFontFamily('Inter', '800'), // Assuming this function returns a string
-    fontWeight: 'bold',
+    fontFamily: getFontFamily("Inter", "800"), // Assuming this function returns a string
+    fontWeight: "bold",
   },
 });
 
